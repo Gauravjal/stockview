a930d067af25e857bf1d1941365ee1d77936389e
Gauravjal <91470651+Gauravjal@users.noreply.github.com>
2023-06-09 12:03:15 +0530

    updated production url

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
@@ -39,7 +39,7 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, "logs.log"), {
 });
 
 //Get request
-app.get("/", (req, res) => res.send("API running"));
+//app.get("/", (req, res) => res.send("API running"));
 //app.use(notFoundMiddleware);
 //Define routes for users,auth,profile,posts respectively
 
